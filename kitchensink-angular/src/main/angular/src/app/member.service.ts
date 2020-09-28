3a42f0d423b86836904eb081198fea2a5c9a7cff
Tomas Aronsson <d0malaga@gmail.com>
2020-09-28 22:52:11 +0200

    kitchensink-angular - app work from kubernetes

--- kitchensink-angular/src/main/angular/src/app/member.service.ts
+++ kitchensink-angular/src/main/angular/src/app/member.service.ts
@@ -13,11 +13,13 @@ import { environment } from '../environments/environment';
 export class MemberService {
 
   private membersUrlFile = 'assets/members2.json';  // URL to web api
-  // Fails because webapp served from another server than API
+
+  // Fails because webapp served from another server
+  // unless the server add CORS for the api
   private membersUrlCORS = environment.API_URL + 'members'
-  // private membersUrl = location.origin + this.locationStrategy.getBaseHref() + '/rest';
-  private membersUrlTry = this.locationStrategy.getBaseHref() + '/rest/members';
-  private membersUrl = environment.API_URL + 'members'
+
+  // Relative path works when deployed with the rest API
+  private membersUrl = './rest/members';
 
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -25,8 +27,7 @@ export class MemberService {
 
   constructor(
     private http: HttpClient,
-    private messageService: MessageService,
-    private locationStrategy: LocationStrategy
+    private messageService: MessageService
   ) {}
 
   /** GET all members */
