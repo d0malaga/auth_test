d081d21b3fb22fe6160fa5832e807148348c5bf7
Tomas Aronsson <d0malaga@gmail.com>
2020-11-10 13:32:00 +0100

    keycloak quickstart: monitoring, docker-compose working, Grafana not configured

---
+++ app-jee-html5-prometheus/prometheus/src/main/resources/wrapper_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -z "${WILDFLY_METRICS_URL}" ]; then
+    echo "you must define \$WILDFLY_METRICS_URL"
+    exit 1
+fi
+
+echo "patching config file: /tmp/prometheus.yml -> ${WILDFLY_METRICS_URL}"
+sed -i "s/\$WILDFLY_METRICS_URL/${WILDFLY_METRICS_URL}/g" /tmp/prometheus.yml
+
+echo /bin/prometheus --config.file=/tmp/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles
+/bin/prometheus --config.file=/tmp/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles
