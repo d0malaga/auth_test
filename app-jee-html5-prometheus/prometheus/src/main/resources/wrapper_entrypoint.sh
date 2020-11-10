8209ea85aea728087f973a224b86d28a92358a4b
Tomas Aronsson <d0malaga@gmail.com>
2020-11-10 15:01:39 +0100

    keycloak quickstart: monitoring, basic k8s working, Grafana not configured

--- app-jee-html5-prometheus/prometheus/src/main/resources/wrapper_entrypoint.sh
+++ app-jee-html5-prometheus/prometheus/src/main/resources/wrapper_entrypoint.sh
@@ -6,7 +6,7 @@ if [ -z "${WILDFLY_METRICS_URL}" ]; then
 fi
 
 echo "patching config file: /tmp/prometheus.yml -> ${WILDFLY_METRICS_URL}"
-sed -i "s/\$WILDFLY_METRICS_URL/${WILDFLY_METRICS_URL}/g" /tmp/prometheus.yml
+sed -i "s;\$WILDFLY_METRICS_URL;${WILDFLY_METRICS_URL};g" /tmp/prometheus.yml
 
 echo /bin/prometheus --config.file=/tmp/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles
 /bin/prometheus --config.file=/tmp/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles
